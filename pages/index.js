70c794910025894a408361c1da23c0d22bb0378b
Nikhil Kadyan <nikhilkadyan@Nicks-MacBook-Air.local>
2022-04-07 14:46:18 +0530

    removed next js image in favour of tailwind styles

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Layout from '../components/layout';
 
 const products = [
@@ -48,7 +47,7 @@ export default function Home() {
 						{products.map((product) => (
 							<a key={product.id} href={product.href} className="group">
 								<div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-									<Image
+									<img
 										src={product.imageSrc}
 										alt={product.imageAlt}
 										className="w-full h-full object-center object-cover group-hover:opacity-75"
